12f01c6b29c01f6877533d168ec4eccf2ab4beb6
Leonardo Carbonari Dela Torre <carbonarileo@hotmail.com>
2021-01-21 14:55:13 -0300

    Teste de raids de 1h em 1h

--- data/scripts/globalevents/spawn/raids.lua
+++ data/scripts/globalevents/spawn/raids.lua
@@ -1,72 +1,26 @@
 local raids = {
-	-- Weekly
-	--Segunda-Feira
-	['Monday'] = {
-		['06:00'] = {raidName = 'RatsThais'},
-	},
-
-	--Terça-Feira
-	['Tuesday'] = {
-		['16:00'] = {raidName = 'Midnight Panther'}
-	},
-
-	--Quarta-Feira
-	['Wednesday'] = {
-		['12:00'] = {raidName = 'Draptor'},
-		['21:25'] = {raidName = 'Orshabaal'},
-	},
-
-	--Quinta-Feira
-	['Thursday'] = {
-		['19:00'] = {raidName = 'Undead Cavebear'}
-	},
-
-	--Sexta-feira
-	['Friday'] = {
-		['06:00'] = {raidName = 'Titanica'}
-	},
-
-	--Sábado
-	['Saturday'] = {
-		['20:00'] = {raidName = 'Draptor'}
-	},
-
-	--Domingo
-	['Sunday'] = {
-		['15:00'] = {raidName = 'Midnight Panther'},
-		['13:00'] = {raidName = 'Orc Backpack'}
-	},
-
-	-- By date (Day/Month)
-	['31/10'] = {
-		['16:00'] = {raidName = 'Halloween Hare'}
-	}
+	[1] = 'Orhsabaal',
+	[2] = 'Morgaroth',
+	[3] = 'Ghazbaran',
+	[4] = 'Ferumbras',
+	[5] = 'Mawhawk',
+	[6] = 'Orc Backpack',
+	[7] = 'Citizen',
+	[8] = 'Draptor',
+	[9] = 'Necropharus',
+	[10] = 'Yeti'
 }
 
 local spawnRaids = GlobalEvent("spawn raids")
 function spawnRaids.onThink(interval, lastExecution, thinkInterval)
-	local day, date = os.date('%A'), getRealDate()
+	selectedRaid = raids[math.random(1, 10)];
 
-	local raidDays = {}
-	if raids[day] then
-		raidDays[#raidDays + 1] = raids[day]
-	end
-	if raids[date] then
-		raidDays[#raidDays + 1] = raids[date]
-	end
-	if #raidDays == 0 then
-		return true
+	if selectedRaid then
+		Game.startRaid(selectedRaid)
 	end
 
-	for i = 1, #raidDays do
-		local settings = raidDays[i][getRealTime()]
-		if settings and not settings.alreadyExecuted then
-			Game.startRaid(settings.raidName)
-			settings.alreadyExecuted = true
-		end
-	end
 	return true
 end
 
-spawnRaids:interval(60000)
+spawnRaids:interval(3600000)
 spawnRaids:register()
