a2a3a2cb1e3de01eb1ea02a3e86951c1d9df5ea7
Leonardo Carbonari Dela Torre <carbonarileo@hotmail.com>
2021-01-30 20:57:38 -0300

    Espaçando as raids de Deepling
    Forçando cada raid de Jaul, Obujos e Tanjis a ter um intervalo de 5 minutos entre cada uma para ver se todas acontecem.

--- data/scripts/globalevents/spawn/raids.lua
+++ data/scripts/globalevents/spawn/raids.lua
@@ -15,18 +15,22 @@ local timedRaids = {
 	};
 };
 
+local minutesSinceLastRaid = 0;
 local spawnRaids = GlobalEvent("spawn raids")
 function spawnRaids.onThink(interval, lastExecution, thinkInterval)
 	local day = os.date('%A')
 
 	if timedRaids[day] then
 		for i = 1, #timedRaids[day] do
-			if not timedRaids[day][i].execute_time then
+			if not timedRaids[day][i].execute_time and minutesSinceLastRaid >= 5 then
+				minutesSinceLastRaid = 0;
 				Game.startRaid(timedRaids[day][i].name)
 				timedRaids[day][i].execute_time = os.time();
 			end
 		end
 	end
+
+	minutesSinceLastRaid = minutesSinceLastRaid + 1;
 
 	return true
 end
