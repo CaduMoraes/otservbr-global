f5710c385bb04c3e049a197bbe8ac52088f94fc0
Leonardo Carbonari Dela Torre <carbonarileo@hotmail.com>
2021-02-03 19:43:33 -0300

    Correção para raids de Deeplings
    Tentando fazer com que as invasões ocorram sempre as 19h ou mais das quartas-feiras.

--- data/scripts/globalevents/spawn/raids.lua
+++ data/scripts/globalevents/spawn/raids.lua
@@ -1,32 +1,41 @@
 local timedRaids = {
-	['Wednesday'] = {
-		{
-			name = 'Jaul',
-			execute_time = nil,
-		},
-		{
-			name = 'Obujos',
-			execute_time = nil,
-		},
-		{
-			name = 'Tanjis',
-			execute_time = nil,
-		},
+	['Wednesday'] = {			-- Day of the week
+		['19'] = {				-- Hour
+			{
+				name = 'Jaul',
+				execute_time = nil,
+			},
+			{
+				name = 'Obujos',
+				execute_time = nil,
+			},
+			{
+				name = 'Tanjis',
+				execute_time = nil,
+			},
+		}
 	};
 };
+
+-- This function checks the timedRaids table (yes, table, because in Lua associative arrays are called tables)
+-- to look for raids to happen. If the current day matches a record on the table and the current hour
+-- is higher or equal to the one on the table, the invasion happens. If there is more than one invasion
+-- inside the hours key of the table, they will occur every five minutes starting from the first one.
 
 local minutesSinceLastRaid = 0;
 local spawnRaids = GlobalEvent("spawn raids")
 function spawnRaids.onThink(interval, lastExecution, thinkInterval)
-	local day = os.date('%A')
+	local day, hour = os.date('%A'), os.date('%H');
 
 	if timedRaids[day] then
-		for i = 1, #timedRaids[day] do
-			if not timedRaids[day][i].execute_time and minutesSinceLastRaid >= 5 then
+		local i = 1;
+		for k,v in pairs(timedRaids[day]) do
+			if not k[i].execute_time and minutesSinceLastRaid >= 5 and k <= hour then
 				minutesSinceLastRaid = 0;
-				Game.startRaid(timedRaids[day][i].name)
-				timedRaids[day][i].execute_time = os.time();
+				Game.startRaid(k[i].name)
+				k[i].execute_time = os.time();
 			end
+			i = i + 1;
 		end
 	end
 
